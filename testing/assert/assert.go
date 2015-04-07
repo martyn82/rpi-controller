bf8435e9420b178ce1a620d9af116fab68e63236
Martijn Endenburg <martijn.endenburg@gmail.com>
2015-04-07 10:06:25 +0200

    Add test for testing lib

--- testing/assert/assert.go
+++ testing/assert/assert.go
@@ -5,38 +5,54 @@ import (
     "testing"
 )
 
+func isNil(value interface{}) bool {
+    return value == nil
+}
+
+func isTrue(value interface{}) bool {
+    return value == true
+}
+
+func isEqual(value1 interface{}, value2 interface{}) bool {
+    return value1 == value2
+}
+
+func isType(value1 interface{}, value2 interface{}) bool {
+    return reflect.TypeOf(value1) == reflect.TypeOf(value2)
+}
+
 func Nil(t *testing.T, actual interface{}) {
-    if actual != nil {
+    if !isNil(actual) {
         t.Errorf("Failed to assert that argument is NIL:\n%q", actual)
     }
 }
 
 func NotNil(t *testing.T, actual interface{}) {
-    if actual == nil {
+    if isNil(actual) {
         t.Errorf("Failed to assert that argument is not NIL:\n%q", actual)
     }
 }
 
 func True(t *testing.T, actual interface{}) {
-    if actual != true {
+    if !isTrue(actual) {
         t.Errorf("Failed to assert that argument is True:\n%q", actual)
     }
 }
 
 func False(t *testing.T, actual interface{}) {
-    if actual != false {
+    if isTrue(actual) {
         t.Errorf("Failed to assert that argument is False:\n%q", actual)
     }
 }
 
 func Equals(t *testing.T, expected interface{}, actual interface{}) {
-    if actual != expected {
+    if !isEqual(expected, actual) {
         t.Errorf("Failed to assert that two arguments are equal:\nexpected:\n%q\nactual:\n%q", expected, actual)
     }
 }
 
 func Type(t *testing.T, expected interface{}, actual interface{}) {
-    if reflect.TypeOf(actual) != reflect.TypeOf(expected) {
+    if !isType(actual, expected) {
         t.Errorf("Failed to assert that two arguments are of the same type:\nexpected:\n%T\nactual:\n%T", expected, actual)
     }
 }
---
+++ testing/assert/assert_test.go
@@ -0,0 +1,53 @@
+package assert
+
+import (
+    "testing"
+)
+
+func TestAssertNil(t *testing.T) {
+    if !isNil(nil) {
+        t.Errorf("Expected nil to be nil.")
+    }
+
+    Nil(t, nil)
+}
+
+func TestAssertNotNil(t *testing.T) {
+    if isNil("" ) {
+        t.Errorf("Expected empty string to be not nil.")
+    }
+
+    NotNil(t, "")
+}
+
+func TestAssertTrue(t *testing.T) {
+    if !isTrue(true) {
+        t.Errorf("Expected true to be true.")
+    }
+
+    True(t, true)
+}
+
+func TestAssertFalse(t *testing.T) {
+    if isTrue(false) {
+        t.Errorf("Expected false to be not true.")
+    }
+
+    False(t, false)
+}
+
+func TestAssertEquals(t *testing.T) {
+    if !isEqual("", "") {
+        t.Errorf("Expected two empty strings to be equal.")
+    }
+
+    Equals(t, nil, nil)
+}
+
+func TestAssertType(t *testing.T) {
+    if !isType(1, 121) {
+        t.Errorf("Expected two integers to be of equal type.")
+    }
+
+    Type(t, nil, nil)
+}
